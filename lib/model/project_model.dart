1dd7c77a2da0189440a7a564d1caa97f57b90b26
Pritesh <pritesh@captus-technologies.com>
2025-05-30 15:12:47 +0530

    Minor Design Changes

--- lib/model/project_model.dart
+++ lib/model/project_model.dart
@@ -17,48 +17,48 @@ List<Project> projectList = [
     'NewWave Harbor',
     'NewWave Harbor Operator App is a boat rental management platform designed for fleet operators to handle bookings, schedules, and vessel tracking efficiently.Operators can manage boat listings, assign captains, and monitor trips in realtime. The app streamlines rental operations with features like payment tracking, maintenance logging, and GPS integration.',
     'assets/images/newwave_harbor.jpeg',
-    'https://play.google.com/store/apps/details?id=com.wooCommerce',
+    '',
   ),
   Project(
       'Woo commerce App',
       'This app provides the ultimate online product purchase with their customers using PayPal or Another technic.Users can register and log in to the application. It views all the products in the app.',
-      'assets/images/alarm.jpg',
-      ''
+      'assets/images/woocommerce.jpeg',
+      'https://play.google.com/store/apps/details?id=com.wooCommerce'
   ),
   Project(
-      'CUI Aider',
-      'CUI AIDER is an extraordinary application that brings together augmented reality (AR), a robust student portal, intuitive class management, and a comprehensive GPA system, providing an all-encompassing solution for students.Using AR technology, CUI AIDER offers an immersive experience, allowing users to explore the complete university campus virtually. Through AR, students can visualize buildings, navigate with interactive directions, and gain a better understanding of their surroundings.',
-      'assets/images/cui.png',
-      ''
+      'SSA Driver App',
+      'This is the application for drivers who provide transportation services toGujaratGovernment Schools under Sarva Shiksha Abhiyan (SSA). An authorized driver will be able to access this application and all their trips will be recorded. The driver will take attendance of all students and they can see their earnings in the application. They will be tracked and notified in case of excessive speed and off-route driving. The SOS Call feature of the App will be helpful in case of emergencies.',
+      'assets/images/ssa.jpeg',
+      'https://play.google.com/store/apps/details?id=com.ssaapplication'
   ),
   Project(
-      'Flutter Music Streaming Application',
-      'A Flutter music streaming application that allows users to play audio files from local storage. The app features a beautiful neumorphic UI design and provides a smooth user experience for browsing and playing music.',
-      'assets/images/player.png',
-      ''
+      'Stable Hub',
+      'Give your owners a first-class ownership experience with STABLE HUB! Trainer to owner communication at its very best.Send all your owners to secure and private text, photos, video, and audio updates in seconds...Trainer, Pre-Trainer, Breaker, Syndicate, or Stud you can personalize your updates with your own logo or banner and send personalized and professional updates to owners effortlessly and directly. STABLE HUB has a wealth of knowledge and experience to help keep your owners up to date with all the latest information on their horses.',
+      'assets/images/stable_hub.jpeg',
+      'https://play.google.com/store/apps/details?id=com.stablehub'
   ),
   Project(
-      'Food Recipe App',
-      'This repository contains a Flutter implementation of a Food Recipe app with a captivating introduction section and impressive animations. It also features a signup and signin page with animations, elegantly presented on a bottom sheet.',
-      'assets/images/recipe.png',
-      ''
+      'ObvioHealth',
+      'This is an organization that handles medical clinical trials. These applications are used to collect data from Participants and guide them for medication as well as medical shipments.',
+      'assets/images/obvio_health.jpeg',
+      'https://play.google.com/store/apps/developer?id=ObvioHealth'
   ),
   Project(
-      'Task Sync Pro',
-      'Welcome to the Beautiful Task Scheduler App repository! This Flutter-based task management application combines elegant design with a robust backend, ensuring a seamless and organized task management experience. From stunning UI to real-time synchronization, this app has you covered.',
-      'assets/images/task.png',
-      ''
+      'Flutter - My Life Compass',
+      'My Life Compass by Ensele - Create a Profile andGet your daily Forecasts. Schedule an appointment with us in order to get detailed future forecasts or consultation for planning any event on an auspicious date and time',
+      'assets/images/my_life_compass.jpeg',
+      'https://play.google.com/store/apps/details?id=com.ensele.ensele'
   ),
   Project(
-      'Flutter Chat Application with Firebase',
-      'Welcome to our innovative Flutter chat application! This feature-rich messaging platform allows users to connect and communicate seamlessly through text and images. The app is built using Flutter for the frontend and integrates with Firebase for backend services, including authentication, real-time database, and storage.',
-      'assets/images/chat.png',
-      ''
+      'Flutter - Worktop Warehouse',
+      'WorktopWarehouseUK provides affordable, professional, and elegant worktops to customers across Shrewsbury,Wirral, Plymouth, and Guild ford. Provide marbles and kitchenware sanitary items for customers. Complete every job from survey to installation within just 7 days.',
+      'assets/images/worktop.jpeg',
+      'https://apps.apple.com/ca/app/worktop-warehouse/id6445984187'
   ),
   Project(
-      'Doctor Appointment Application',
-      'Introducing the extraordinary "Doctor Appointment System" - a state-of-the-art Flutter UI application that redefines healthcare accessibility and efficiency. Seamlessly crafted, this app empowers users to effortlessly select doctors based on categories, engage in smooth messaging, and access detailed profiles.',
-      'assets/images/doctor.png',
-      ''
+      'Flutter - Docklyne',
+      'TheDocklyne ecosystem will make boating more accessible for the modern consumer by securing reliable connections with trusted operations, enhancing communication.',
+      'assets/images/docklyne.jpeg',
+      'https://play.google.com/store/apps/details?id=com.docklyne.marina'
   ),
 ];
